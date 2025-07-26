67ef1964d95d59f9c6902900bf781aee5540e1d7
tsitohaina23 <tsitohaina.rajoelisolo@gmail.com>
2025-07-26 13:58:35 +0300

    Update funny_alarm.py
    niova

--- backend/funny_alarm.py
+++ backend/funny_alarm.py
@@ -1,18 +1,23 @@
-from flask import Flask, request, send_file
+from flask import Flask, send_file
 from gtts import gTTS
 from pydub import AudioSegment
 import os
 from io import BytesIO
+import random
 
 app = Flask(__name__)
 
-@app.route("/audio", methods=["POST"])
-def audio():
-    data = request.get_json()
-    if not data or "text" not in data:
-        return {"erreur": "Pas de text."}, 400
+phrases = [
+    "Bonjour, je suis une voix rigolote !",
+    "Attention, ceci est un message important.",
+    "Tu as gagné un ticket pour la planète Mars !",
+    "Il est temps de faire une pause café.",
+    "Je chante sous la douche, tu veux écouter ?"
+]
 
-    text = data["text"]
+@app.route("/audio", methods=["GET"])
+def audio():
+    text = random.choice(phrases)
     tts = gTTS(text=text, lang="fr")
     tts.save("voix.mp3")
 
@@ -33,8 +38,8 @@ def audio():
         buffer,
         as_attachment=False,
         mimetype="audio/mpeg",
-        download_name="ahaha.mp3"
+        download_name="voix_souriante.mp3"
     )
 
 if __name__ == "__main__":
-    app.run(debug=True, port=2000)
+    app.run(debug=True, port=2000, host="0.0.0.0")
